3788510ea1a75028bfd80825b059f7024ac858f6
Nature Melody <votansang1711@gmail.com>
2026-02-06 17:16:58 +0700

    feat: setup main routing and static informational pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import { Routes, Route } from "react-router-dom";
 import { ProductPage } from "@/pages/ProductPage";
 import { ProductDetailPage } from "@/pages/ProductDetailPage";
+import { Home } from "@/pages/Home";
+import { About } from "@/pages/About";
+import { Contact } from "@/pages/Contact";
+import { CartPage } from "@/pages/CartPage";
+import { UserLayout } from "./layouts/UserLayout";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<ProductPage />} />
-      <Route path="/products" element={<ProductPage />} />
-      <Route path="/products/:id" element={<ProductDetailPage />} />
+      <Route element={<UserLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/products">
+          <Route index element={<ProductPage />} />
+          <Route path=":id" element={<ProductDetailPage />} />
+        </Route>
+        <Route path="/cart" element={<CartPage />} />
+      </Route>
     </Routes>
   );
 }
