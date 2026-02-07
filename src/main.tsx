4137ef8d7fb0fbd15e77af9095017121a11d1568
Nature Melody <votansang1711@gmail.com>
2026-02-07 19:53:44 +0700

    chore: update entry point and global styles with linter configurations

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { CartProvider } from "./context/CartContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* Wrap the App component with BrowserRouter to enable routing functionality */}
     <BrowserRouter>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </BrowserRouter>
   </StrictMode>,
 );
